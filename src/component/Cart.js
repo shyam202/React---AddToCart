3b84309f14fa5657cde950d7399e4feea2bfe460
shyam202 <lakhanishyam11@gmail.com>
2022-04-20 15:59:32 +0200

    Context API & useContext Hook

--- src/component/Cart.js
+++ src/component/Cart.js
@@ -1,45 +1,16 @@
-import React, { useState } from "react";
+import React, { createContext } from "react";
 import "./cart.css";
-import { Scrollbars } from "react-custom-scrollbars";
-import Items from "./Items";
+
 import { products } from "./product";
+import ContextCart from "./ContextCart";
+
+export const CartContext = createContext();
 
 const Cart = () => {
-  const [item, setItem] = useState(products);
-
   return (
-    <>
-      <header>
-        <div className="continue-shopping">
-          <img src="./Images/arrow.png" alt="arrow" className="arrow-icon" />
-          <h3>continue shopping</h3>
-        </div>
-        <div className="cart-icon">
-          <img src="./Images/cart.png" alt="cart" className="cart-icon" />
-          <p>7</p>
-        </div>
-      </header>
-      <section className="main-cart-section">
-        <h1>Shopping Cart</h1>
-        <p className="total-items">
-          You have <span className="total-items-count">7</span> items in your
-          cart
-        </p>
-        <div className="cart-items-container">
-          <Scrollbars>
-            {item.map((curr) => {
-              return <Items key={curr.id} {...curr} />;
-            })}
-          </Scrollbars>
-        </div>
-        <div className="card-total">
-          <h3>
-            Cart Total : <span>1000$</span>
-          </h3>
-          <button>Checkout</button>
-        </div>
-      </section>
-    </>
+    <CartContext.Provider value={products}>
+      <ContextCart />
+    </CartContext.Provider>
   );
 };
 
---
+++ src/component/ContextCart.js
@@ -0,0 +1,44 @@
+import React, { useContext } from "react";
+import { Scrollbars } from "react-custom-scrollbars";
+import { CartContext } from "./Cart";
+import Items from "./Items";
+
+const ContextCart = () => {
+  const item = useContext(CartContext);
+  return (
+    <>
+      <header>
+        <div className="continue-shopping">
+          <img src="./Images/arrow.png" alt="arrow" className="arrow-icon" />
+          <h3>continue shopping</h3>
+        </div>
+        <div className="cart-icon">
+          <img src="./Images/cart.png" alt="cart" className="cart-icon" />
+          <p>7</p>
+        </div>
+      </header>
+      <section className="main-cart-section">
+        <h1>Shopping Cart</h1>
+        <p className="total-items">
+          You have <span className="total-items-count">7</span> items in your
+          cart
+        </p>
+        <div className="cart-items-container">
+          <Scrollbars>
+            {item.map((curr) => {
+              return <Items key={curr.id} {...curr} />;
+            })}
+          </Scrollbars>
+        </div>
+        <div className="card-total">
+          <h3>
+            Cart Total : <span>1000$</span>
+          </h3>
+          <button>Checkout</button>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default ContextCart;
